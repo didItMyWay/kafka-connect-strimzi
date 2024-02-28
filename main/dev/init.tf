6208cf295704aa72fb6be003cc09070fe5dfd1e0
Neha Fathima <151570537+itsFathima@users.noreply.github.com>
2024-02-28 16:11:35 +0100

    Update init.tf

--- main/dev/init.tf
+++ main/dev/init.tf
@@ -6,18 +6,10 @@ terraform {
   }
   required_providers {
     aws   = "~> 5.0"
-    aiven = {
-      source  = "aiven/aiven"
-      version = ">= 3.2.1"
-    }
 
   }
 }
 
 provider "aws" {
-  region = "eu-central-1"
-}
-
-provider "aiven" {
-  api_token = data.aws_secretsmanager_secret_version.TokenValue.secret_string
+  region = "XXX"
 }
