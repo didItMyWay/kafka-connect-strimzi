03b30f00aaacd9b63f8f88e9fc0d953a0f139cfe
Neha Fathima <151570537+itsFathima@users.noreply.github.com>
2024-02-28 16:11:45 +0100

    Update init.tf

--- main/dev/init.tf
+++ main/dev/init.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "my-dev-bucket"
     key    = "ingestion/dev"
-    region = "eu-central-1"
+    region = "XXX"
   }
   required_providers {
     aws   = "~> 5.0"
